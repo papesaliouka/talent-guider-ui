698b14c58f9d0b5e50c900507b0dccba1965498b
papesaliouka <papesaliouka@gmail.com>
2023-09-03 12:17:25 +0000

    finally found good one

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import {makeTriplles,
   makeDurationBySubject,
   makeTraffic} from 'src/utils/overview-helpers';
 
-const API_URL = process.env.API_URL 
+const API_URL = process.env.NEXT_PUBLIC_API_URL 
 
 
 const Page = () =>{
