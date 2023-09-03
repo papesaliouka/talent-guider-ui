6d139796046a87ff835c54e1a6c0049420c828ff
papesaliouka <papesaliouka@gmail.com>
2023-09-03 12:04:43 +0000

    removed localhost values

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,7 @@ import {makeTriplles,
   makeDurationBySubject,
   makeTraffic} from 'src/utils/overview-helpers';
 
-const API_URL = process.env.API_URL || 'http://localhost:8000'
-
+const API_URL = process.env.API_URL 
 
 
 const Page = () =>{
