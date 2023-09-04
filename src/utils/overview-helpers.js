a0d30bbe1f4b74f222218e768b3016cd94709a18
papesaliouka <papesaliouka@gmail.com>
2023-09-04 02:17:05 +0000

    adapted categories to mongo

--- src/utils/overview-helpers.js
+++ src/utils/overview-helpers.js
@@ -1,11 +1,11 @@
   const numDaysMap = {
-    1: 'Mon',
-    2: 'Tue',
-    3: 'Wed',
-    4: 'Thu',
-    5: 'Fri',
-    6: 'Sat',
-    7: 'Sun'
+    2: 'Mon',
+    3: 'Tue',
+    4: 'Wed',
+    5: 'Thu',
+    6: 'Fri',
+    7: 'Sat',
+    1: 'Sun'
   };
 
 export const makeTriplles = (days, durations, subjectNames, dates) => {
