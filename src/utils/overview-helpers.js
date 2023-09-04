746e9b2d4d3788e6601014053fc6d44cd3f3c6b3
papesaliouka <papesaliouka@gmail.com>
2023-09-04 01:29:38 +0000

    fixed day categories map

--- src/utils/overview-helpers.js
+++ src/utils/overview-helpers.js
@@ -1,11 +1,11 @@
   const numDaysMap = {
-    1: 'Mon',
-    2: 'Tue',
-    3: 'Wed',
-    4: 'Thu',
-    5: 'Fri',
-    6: 'Sat',
-    7: 'Sun'
+    0: 'Mon',
+    1: 'Tue',
+    2: 'Wed',
+    3: 'Thu',
+    4: 'Fri',
+    5: 'Sat',
+    6: 'Sun'
   };
 
 export const makeTriplles = (days, durations, subjectNames, dates) => {
